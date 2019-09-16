197657775b4871b9e154d72b22eba42c8271d3d0
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2019-09-16 19:44:19 +0600

    Implemented lambda
    Implemented lambda with Linq

--- Features/Program.cs
+++ Features/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Features
 {
@@ -6,7 +8,26 @@ namespace Features
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Array of developers of Employee Type
+            Employee[] developers = new Employee[]
+            {
+                new Employee { Id=1,Name="Scott"},
+                new Employee { Id=2,Name="Chris"}
+            };
+
+            //Array of sales persons of Employee Type
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id=3,Name="Alex"}
+            };
+
+            Console.WriteLine(developers.Count());
+
+            foreach (var employee in developers.Where(e => e.Name.StartsWith('S')))
+            {
+                Console.WriteLine(employee.Name);
+            }
+
         }
     }
 }
