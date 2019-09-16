f8629e89ae41a95f39ac784910012981b5a6a367
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2019-09-16 20:45:14 +0600

    Implemented Func

--- Features/Program.cs
+++ Features/Program.cs
@@ -8,6 +8,17 @@ namespace Features
     {
         static void Main(string[] args)
         {
+            Func<int, int> Square = x => x * x;
+            //Alternate local methid
+            //int Square(int x) => x * x;
+            Console.WriteLine($"Square of 5 is: {Square(5)}");
+
+            Func<int, int, int> Add = (x, y) => x + y;
+            Console.WriteLine($"SUM of 5 & 7 is: {Add(5, 7)}");
+
+            Action<int> write = x => Console.WriteLine(x);
+            write(Square(Add(4, 4)));
+
             //Array of developers of Employee Type
             Employee[] developers = new Employee[]
             {
@@ -21,13 +32,13 @@ namespace Features
                 new Employee { Id=3,Name="Alex"}
             };
 
-            Console.WriteLine(developers.Count());
+            Console.WriteLine($"Total number of developers is: {developers.Count()}");
 
-            foreach (var employee in developers.Where(e => e.Name.StartsWith('S')))
+            foreach (var employee in developers.Where(e => e.Name.Length == 5)
+                                               .OrderBy(e => e.Name))
             {
                 Console.WriteLine(employee.Name);
             }
-
         }
     }
 }
