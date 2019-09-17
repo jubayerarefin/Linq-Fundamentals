c577f92f18154b226480172bcdc637ec63aa42ed
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2019-09-17 10:59:59 +0600

    Updated with different methods of Linq query

--- Features/Program.cs
+++ Features/Program.cs
@@ -9,7 +9,7 @@ namespace Features
         static void Main(string[] args)
         {
             Func<int, int> Square = x => x * x;
-            //Alternate local methid
+            //Alternate local function
             //int Square(int x) => x * x;
             Console.WriteLine($"Square of 5 is: {Square(5)}");
 
@@ -17,6 +17,8 @@ namespace Features
             Console.WriteLine($"SUM of 5 & 7 is: {Add(5, 7)}");
 
             Action<int> write = x => Console.WriteLine(x);
+            //Alternate local function
+            //void write(int x) => Console.WriteLine(x);
             write(Square(Add(4, 4)));
 
             //Array of developers of Employee Type
@@ -34,8 +36,22 @@ namespace Features
 
             Console.WriteLine($"Total number of developers is: {developers.Count()}");
 
-            foreach (var employee in developers.Where(e => e.Name.Length == 5)
-                                               .OrderBy(e => e.Name))
+            var query_1 = developers.Where(e => e.Name.Length == 5)
+                                    .OrderBy(e => e.Name);
+
+            var query_2 = from developer in developers
+                          where developer.Name.Length == 5
+                          orderby developer.Name
+                          select developer;
+
+            foreach (var employee in query_1)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            Console.WriteLine("\n***\n");
+
+            foreach (var employee in query_2)
             {
                 Console.WriteLine(employee.Name);
             }
