028b919c1067a3fe302be62f60646cc4ddb59451
Jubayer Arefin <jubayerarefin@users.noreply.github.com>
2019-09-17 19:35:27 +0600

    Performed more Linq queries with lambda on data from csv

--- Cars/Program.cs
+++ Cars/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Cars
 {
@@ -6,7 +9,81 @@ namespace Cars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cars = ProcesFile("fuel.csv");
+            //var query_1 = cars.OrderByDescending(c => c.Combined).ThenBy(c => c.Name);
+            var query_1 = from car in cars
+                          where car.Manufacturer == "BMW" && car.Year == 2016
+                          orderby car.Combined descending, car.Name ascending
+                          select new
+                          {
+                              car.Manufacturer,
+                              car.Name,
+                              car.Combined
+                          };
+            var query_2 = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                              .OrderByDescending(c => c.Combined)
+                              .ThenBy(c => c.Name)
+                              .Select(c => c);
+            var top = cars.Where(c => c.Manufacturer == "BMW" && c.Year == 2016)
+                              .OrderByDescending(c => c.Combined)
+                              .ThenBy(c => c.Name)
+                              .Select(c => c)
+                              .First();
+            Console.WriteLine($"{top.Name} : {top.Combined}");
+
+            var result_1 = cars.Any(c => c.Manufacturer == "Ford");
+
+            var result_2 = cars.SelectMany(c => c.Name);
+
+            foreach (var character in result_2)
+            {
+                Console.WriteLine(character);
+            }
+
+            Console.WriteLine($"{result_1}");
+
+            foreach (var car in query_2)
+            {
+                Console.WriteLine($"{car.Name} : {car.Combined}");
+            }
+        }
+
+        private static List<Car> ProcesFile(string path)
+        {
+            var query = File.ReadAllLines(path)
+                .Skip(1)
+                .Where(l => l.Length > 1)
+                .ToCar();
+            return query.ToList();
+            /*
+            File.ReadAllLines(path)
+            .Skip(1)
+            .Where(line => line.Length > 1)
+            .Select(Car.ParseFromCSV)
+            .ToList();
+            */
+        }
+    }
+    public static class CarExtensions
+    {
+        public static IEnumerable<Car> ToCar(this IEnumerable<string> source)
+        {
+            foreach (var line in source)
+            {
+                var columns = line.Split(",");
+
+                yield return new Car
+                {
+                    Year = int.Parse(columns[0]),
+                    Manufacturer = columns[1],
+                    Name = columns[2],
+                    Displacement = double.Parse(columns[3]),
+                    Cylinders = int.Parse(columns[4]),
+                    City = int.Parse(columns[5]),
+                    Highway = int.Parse(columns[6]),
+                    Combined = int.Parse(columns[7])
+                };
+            }
         }
     }
 }
